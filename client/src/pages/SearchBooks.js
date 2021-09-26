38ca98b1ffe8ec02467cfbb3b2dbcabf38593b66
blanden-hub <83385242+blanden-hub@users.noreply.github.com>
2021-09-26 14:32:59 -0700

    added mutations and queries

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,9 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
-
-import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
-import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import React, { useState, useEffect } from "react";
+import {
+  Jumbotron,
+  Container,
+  Col,
+  Form,
+  Button,
+  Card,
+  CardColumns,
+} from "react-bootstrap";
+import { useMutation } from "@apollo/client";
+import { SAVE_BOOK } from "../utils/mutations";
+import Auth from "../utils/auth";
+import { searchGoogleBooks } from "../utils/API";
+import { saveBookIds, getSavedBookIds } from "../utils/localStorage";
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -13,6 +22,7 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+  const [bookSaved] = useMutation(SAVE_BOOK);
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
@@ -65,11 +75,9 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      await nookSaved({
+        variables: {content: bookToSave},
+      });
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
@@ -116,7 +124,11 @@ const SearchBooks = () => {
             return (
               <Card key={book.bookId} border='dark'>
                 {book.image ? (
-                  <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' />
+                  <Card.Img
+                  src={book.image}
+                  alt={`The cover for ${book.title}`}
+                  variant="top"
+                />
                 ) : null}
                 <Card.Body>
                   <Card.Title>{book.title}</Card.Title>
@@ -124,12 +136,17 @@ const SearchBooks = () => {
                   <Card.Text>{book.description}</Card.Text>
                   {Auth.loggedIn() && (
                     <Button
-                      disabled={savedBookIds?.some((savedBookId) => savedBookId === book.bookId)}
-                      className='btn-block btn-info'
-                      onClick={() => handleSaveBook(book.bookId)}>
-                      {savedBookIds?.some((savedBookId) => savedBookId === book.bookId)
-                        ? 'This book has already been saved!'
-                        : 'Save this Book!'}
+                    disabled={savedBookIds?.some(
+                      (savedBookId) => savedBookId === book.bookId
+                    )}
+                    className="btn-block btn-info"
+                    onClick={() => handleSaveBook(book.bookId)}
+                  >
+                    {savedBookIds?.some(
+                      (savedBookId) => savedBookId === book.bookId
+                    )
+                      ? "This book has already been saved!"
+                      : "Save this Book!"} 
                     </Button>
                   )}
                 </Card.Body>
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,24 +2,43 @@ const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
 type User {
-    _id: ID
-    username: String
+    _id: ID!
+    username: String!
     email: String
+    bookCount: Int
     savedBooks: [Book]
   }
   type Book {
     _id: ID  
     authors: String
+    bookId: ID!
+    authors: [String]
     description: String
     image: String
     link: String
     title: String
   }
   type Query {
-    users: [User]
-    user(username:String!): User  
-    books(username: String): [Book]
+    users: User
   }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(content: bookData!): User
+    removeBook(bookId: String!): User
+  }
+  type Auth {
+    token: ID!
+    user: User! 
+}
+input bookData {
+  bookId: ID!
+  authors: [String]!
+  description: String
+  title: String
+  image: String
+  link: String
+}
 `;
 
 module.exports = typeDefs;
