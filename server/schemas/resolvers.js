4f75bd3fdd6392884887abdad77df887de6457c2
blanden-hub <83385242+blanden-hub@users.noreply.github.com>
2021-09-26 13:53:00 -0700

    got queries up and working

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,42 @@
+const { User, Book } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+
 const resolvers = {
     Query: {
-      brysonTiller: () => {
-        return "Bryson Tiller";
+      users: async () => {
+        return User.find().select("-__v -password");
+      },
+    },
+    Mutation: {
+      addUser: async (parent, args) => {
+        const user = await User.create(args);
+  
+        return user;
+      },
+      login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+  
+        if (!user) {
+          throw new AuthenticationError("Incorrect credentials");
+        }
+  
+        const correctPw = await user.isCorrectPassword(password);
+  
+        if (!correctPw) {
+          throw new AuthenticationError("Incorrect credentials");
+        }
+  
+        return user;
+      },
+      saveBook: async (parent, args) => {
+        const book = await Book.create(args);
+  
+        return book;
+      },
+      removeBook: async (parent, args) => {
+        const book = await Book.create(args);
+  
+        return book;
       },
     },
   };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,25 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-type Query {
-    brysonTiller: String
-}`;
+type User {
+    _id: ID
+    username: String
+    email: String
+    savedBooks: [Book]
+  }
+  type Book {
+    _id: ID  
+    authors: String
+    description: String
+    image: String
+    link: String
+    title: String
+  }
+  type Query {
+    users: [User]
+    user(username:String!): User  
+    books(username: String): [Book]
+  }
+`;
 
 module.exports = typeDefs;
